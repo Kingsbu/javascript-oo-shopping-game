0efafdf69cd00546daa272d3de9210275a021f1e
Nolo Space <nolo@darkstar.example.net>
2020-10-02 12:13:54 +0200

    Loading Master Data

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -16,10 +16,10 @@ let player = {
 
 
 // Define the Product class - write the Constructor function for Product class here
-function Product( id, name, price, expiryDate){
-    this.id  = id;
-    this.name = name;
-    this.price = price;
+function Product( id, name, price, expiryDate){  
+    this.id  = id; 
+    this.name = name; 
+    this.price = price; 
     this.expiryDate = expiryDate;
 }
 // Complete the dateDiff function
@@ -40,7 +40,7 @@ function MagicProduct(id, name, price, expiryDate, points, isBonus){
     this.isBonus = isBonus;
 }
 // Establish inheritance between Product() & MagicProduct() here
-MagicProduct.prototype = Object.create(Product.prototype);
+MagicProduct.prototype = Object.create(Product());
 
 // Define Rating class here
 class Rating {
@@ -68,7 +68,7 @@ const loadProducts = (map, prodId) => {
     let a = new Array();
     try {
         // Call Object.keys() to load the property names of the Product object in to prodKeys array here
-        let prodKeys = [];
+        let prodKeys =  Object.keys(new Product());
 
         let iterator_obj = map.entries();
 
@@ -78,7 +78,7 @@ const loadProducts = (map, prodId) => {
                 const value = item[1];
 
                 // Create and assign an instance of Product to prodObj here
-                let prodObj;
+                let prodObj = new Product();
 
                 if (prodObj != undefined && prodObj != null) {
                     for (let i = 0; i < prodKeys.length; i++) {
@@ -112,7 +112,7 @@ const loadMagicProducts = (map, prodId) => {
     let a = new Array();
     try {
         // Call Object.key() to load the property names of the MagicProduct object in to magProdKeys array here
-        let magProdKeys = [];
+        let magProdKeys = Object.keys(new MagicProduct());
 
         let iterator_obj = map.entries();
 
@@ -122,7 +122,7 @@ const loadMagicProducts = (map, prodId) => {
                 const value = item[1];
 
                 // Create and assign an instance of MagicProduct to prodObj here
-                let magProdObj;
+                let magProdObj = new MagicProduct();
 
                 if (magProdObj != undefined && magProdObj != null) {
                     for (let i = 0; i < magProdKeys.length; i++) {
